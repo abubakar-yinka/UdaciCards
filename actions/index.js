1b2af76fa586f924c205daeca6621de4a03d03a2
Abubakar Ibrahim <64968306+Abubakar-YI@users.noreply.github.com>
2020-07-05 16:57:45 +0100

    Create Actions
    Created Action types, action creators and an asynchronous action creator.

---
+++ actions/index.js
@@ -0,0 +1,54 @@
+import { getDecks } from '../utils/api';
+
+//action types
+export const ADD_DECK = 'ADD_DECK';
+export const RECEIVE_DECKS = 'RECEIVE_DECKS';
+export const REMOVE_DECK = 'REMOVE_DECK';
+export const ADD_CARD = 'ADD_CARD';
+export const CLEAR_DECKS = 'CLEAR_DECKS';
+
+//action creators
+export function addDeck(title) {
+  return {
+    type: ADD_DECK,
+    title
+  };
+}
+
+export function receiveDecks(decks) {
+  return {
+    type: RECEIVE_DECKS,
+    decks,
+  };
+}
+
+export function removeDeck(id) {
+  return {
+    type: REMOVE_DECK,
+    id
+  };
+}
+
+export function addCardToDeck(deckId, card) {
+  return {
+    type: ADD_CARD,
+    deckId,
+    card,
+  };
+}
+
+export function clearCreatedDecks() {
+  return {
+    type: CLEAR_DECKS,
+  };
+}
+
+//asynchronous action creator
+export function handleInitialData() {
+  return dispatch => {
+    return getDecks()
+        .then((decks) => {
+        dispatch(receiveDecks(decks));
+    });
+  };
+}
